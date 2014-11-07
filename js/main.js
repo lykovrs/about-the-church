a54dd512785329c4c2f650dc26a368eab5aa4562
Lykov Roman <rlykov@ufs-online.ru>
2014-11-07 10:01:17 +0300

    create header

---
+++ js/main.js
@@ -0,0 +1,83 @@
+$(function() {
+	var createUser, loginUser, form,
+
+	// From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
+		emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+		name = $( "#name" ),
+		email = $( "#email" ),
+		password = $( "#password" ),
+		allFields = $( [] ).add( name ).add( email ).add( password ),
+		tips = $( ".validateTips" );
+
+	function updateTips( t ) {
+		tips
+			.text( t )
+			.addClass( "ui-state-highlight" );
+		setTimeout(function() {
+			tips.removeClass( "ui-state-highlight", 1500 );
+		}, 500 );
+	}
+
+	function checkLength( o, n, min, max ) {
+		if ( o.val().length > max || o.val().length < min ) {
+			o.addClass( "ui-state-error" );
+			updateTips( "Length of " + n + " must be between " +
+			min + " and " + max + "." );
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	function checkRegexp( o, regexp, n ) {
+		if ( !( regexp.test( o.val() ) ) ) {
+			o.addClass( "ui-state-error" );
+			updateTips( n );
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	createUser = $( "#create-user-form" ).dialog({
+		autoOpen: false,
+		height: 300,
+		width: 350,
+		modal: true,
+		buttons: {
+			Cancel: function() {
+				dialog.dialog( "close" );
+			}
+		},
+		close: function() {
+//					$("#registration").reset();
+//					allFields.removeClass( "ui-state-error" );
+		}
+	});
+
+	loginUser = $( "#login-user-form" ).dialog({
+		autoOpen: false,
+		height: 300,
+		width: 350,
+		modal: true,
+		buttons: {
+			Cancel: function() {
+				dialog.dialog( "close" );
+			}
+		},
+		close: function() {
+//					$("#registration").reset();
+//					allFields.removeClass( "ui-state-error" );
+		}
+	});
+
+
+
+	$( "#createUser" ).button().on( "click", function() {
+		createUser.dialog( "open" );
+	});
+
+	$( "#loginUser" ).button().on( "click", function() {
+		loginUser.dialog( "open" );
+	});
+});
